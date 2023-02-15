ae796aee69f62808d04ff5477001d94eaac6e7e8
RenatoDourad0 <renatocvdourado@hotmail.com>
2023-02-15 11:20:18 -0300

    definição do cenario de teste unitário para serviço de cars

---
+++ tests/unit/Services/Cars.service.test.ts
@@ -0,0 +1,58 @@
+import { expect } from 'chai';
+import sinon from 'sinon';
+import { Model } from 'mongoose';
+import Car from '../../../src/Domains/Car';
+import Icar from '../../../src/Interfaces/ICar';
+import CarService from '../../../src/Services/CarService';
+
+describe('testes do serviço cars', function () {
+  afterEach(function () {
+    sinon.restore();
+  });
+  it('cria um novo car com sucesso', async function () {
+    const input: Icar = {
+      model: 'Marea',
+      year: 2002,
+      color: 'Black',
+      status: true,
+      buyValue: 15.990,
+      doorsQty: 4,
+      seatsQty: 5,
+    };
+    const output: Car = {
+      id: '6348513f34c397abcad040b2',
+      model: 'Marea',
+      year: 2002,
+      color: 'Black',
+      status: true,
+      buyValue: 15.990,
+      doorsQty: 4,
+      seatsQty: 5,
+    };
+
+    sinon.stub(Model, 'create').resolves(output);
+
+    const service = new CarService();
+    const newCar = await service.create(input);
+    expect(newCar).to.be.deep.equal(output);
+  });
+
+  it('lança exessão ao tentar criar um car inválido', async function () {
+    // sem propriedade year
+    const input = {
+      model: 'Marea',
+      color: 'Black',
+      status: true,
+      buyValue: 15.990,
+      doorsQty: 4,
+      seatsQty: 5,
+    };
+
+    try {
+      const service = new CarService();
+      await service.create(input);
+    } catch (error) {
+      expect((error as Error).message).to.be.equal('missing required properties');
+    }
+  });
+});
